952de8882adf1fd87ef0846695e0db37e74b175f
Ashish Chachane <117847834+aashishchachane@users.noreply.github.com>
2022-11-14 14:35:18 +0530

    Add files via upload

---
+++ Odd even.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Enter the number : ";
+	cin>>n;
+	
+	if(n%2==0)
+	{
+		cout<<"The given number is even";
+	}
+	else
+	{
+		cout<<"The given number is odd";
+	}
+	
+	return 0;
+	
+}
